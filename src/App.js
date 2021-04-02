7ded1a525e3416453ac4adea87be0664627089a5
Kevin <Kevin.FNCW@gmail.com>
2021-04-02 15:36:31 -0400

    search bars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: '', // for value in zip search bar
+      city: '', // for value in city search bar
+      zipCodes: [], // list of zip codes gotten from city search bar
+      cities: [], // list of cities gotten from zip search bar
+    }
+  }
+
+  handleZipSearch = (e) => {
+    this.setState({
+      zipCode: e.target.value
+    })
+  }
+
+  handleCitySearch = (e) => {
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+  fetchCities = async () => {
+    // try {
+      let zip = this.state.zipCode;
+      console.log(zip);
+      let response = await axios.get(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      let citiesObject = await response.json();
+      console.log(citiesObject);
+
+      this.setState({
+        cities: citiesObject,
+      });
+
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
+
+  fetchZipCodes = async () => {
+    try {
+      let city = this.state.city.toUpperCase();
+      let response = await axios.get(`http://ctp-zip-api.herokuapp.com/city/${city}`)
+      let zipCodesObject = await response.json();
+      console.log(zipCodesObject);
+
+      this.setState({
+        cities: zipCodesObject,
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Zip Code and City Search</h1>
+        <form>
+          <label>Zip Code: </label>
+          <input
+            type="text" 
+            value={this.state.zipCode}
+            onChange={this.handleZipSearch}
+          />
+          <button type="submit" onClick={this.fetchCities}>Search Zip Code</button>
+        </form>
+
+        <br></br>
+
+        <form>
+          <label>City: </label>
+          <input 
+            type="text"
+            value={this.state.city} 
+            onChange={this.handleCitySearch}
+          />
+          <button type="submit" onClick={this.fetchZipCodes}>Search City</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
